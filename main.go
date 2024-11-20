7632f562eacc8133b47b448c77188696fafb073a
interyx <jtluensmann@gmail.com>
2024-11-20 17:07:49 -0500

    created db files; generated db interface methods

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+import _ "github.com/lib/pq"
 import (
 	"fmt"
 	"github.com/interyx/internal/config"
@@ -15,15 +16,31 @@ type command struct {
 	args []string
 }
 
+type commands struct {
+	names map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.names[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.names[cmd.name]
+	if !ok {
+		return fmt.Errorf("Command %s not found", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
-		return fmt.Errorf("Login requires at least one argument")
+		return fmt.Errorf("Login requires at least one argument\n")
 	}
 	err := s.cfg.SetUser(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("An error occurred: %v", err)
+		return fmt.Errorf("An error occurred: %v\n", err)
 	}
-	fmt.Printf("%s was logged in successfully!", cmd.args[0])
+	fmt.Printf("%s was logged in successfully!\n", cmd.args[0])
 	return nil
 }
 
@@ -33,18 +50,26 @@ func main() {
 	if err != nil {
 		fmt.Printf("An error has occurred: %v\n", err)
 		fmt.Println("Exiting...")
-		os.Exit(0)
+		os.Exit(1)
 	}
-	fmt.Println("Setting user to INTERYX...")
-	err = cfg.SetUser("interyx")
+	thisState := state{
+		cfg: &cfg,
+	}
+	myCommands := commands{}
+	myCommands.names = make(map[string]func(*state, command) error, 5)
+	myCommands.register("login", handlerLogin)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Not enough arguments provided")
+		os.Exit(1)
+	}
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+	err = myCommands.run(&thisState, cmd)
 	if err != nil {
 		fmt.Printf("An error has occurred: %v\n", err)
+		os.Exit(1)
 	}
-	fmt.Println("INTERYX set successfully!")
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Printf("An error has occurred %v\n", err)
-	}
-	fmt.Println("Printing config...")
-	fmt.Println(cfg)
 }
