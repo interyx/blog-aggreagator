794c311d7700012d575630155f856a4d4fd213dd
interyx <jtluensmann@gmail.com>
2024-11-21 16:15:08 -0500

    added fetchFeed() function

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/interyx/internal/config"
 	"github.com/interyx/internal/database"
 	_ "github.com/lib/pq"
-	"os"
-	"time"
 )
 
 type state struct {
@@ -26,6 +31,22 @@ type commands struct {
 	names map[string]func(*state, command) error
 }
 
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.names[name] = f
 }
@@ -36,6 +57,41 @@ func (c *commands) run(s *state, cmd command) error {
 		return fmt.Errorf("Command %s not found", cmd.name)
 	}
 	return handler(s, cmd)
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	// step 1: build the request
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	req.Header.Add("User-Agent", "gator")
+	client := http.Client{}
+	// step 2: execute the request
+	res, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	defer res.Body.Close()
+	// step 3: read the request
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	// step 4: parse the request
+	var feed RSSFeed
+	err = xml.Unmarshal(body, &feed)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+	// step 5: print the data
+	return &feed, nil
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -80,29 +136,37 @@ func handlerRegister(s *state, cmd command) error {
 }
 
 func handlerReset(s *state, cmd command) error {
-  ctx := context.Background()
-  err := s.db.DeleteAllUsers(ctx)
-  if err != nil {
-    return err
-  }
-  fmt.Println("All users have been deleted")
-  return nil;
+	ctx := context.Background()
+	err := s.db.DeleteAllUsers(ctx)
+	if err != nil {
+		return err
+	}
+	fmt.Println("All users have been deleted")
+	return nil
 }
 
 func handlerUsers(s *state, cmd command) error {
-  ctx := context.Background()
-  users, err := s.db.GetAllUsers(ctx)
-  if err != nil {
-    return err
-  }
-  for _, user := range users {
-    fmt.Printf("* %s", user.Name)
-    if user.Name == s.cfg.User {
-      fmt.Printf(" (current)")
-    }
-    fmt.Printf("\n")
-  }
-  return nil
+	ctx := context.Background()
+	users, err := s.db.GetAllUsers(ctx)
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		fmt.Printf("* %s", user.Name)
+		if user.Name == s.cfg.User {
+			fmt.Printf(" (current)")
+		}
+		fmt.Printf("\n")
+	}
+	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	ctx := context.Background()
+	xaml, err := fetchFeed(ctx, "https://wagslane.dev/index.xml")
+	handleError(err)
+	fmt.Printf("%s\n", xaml)
+	return nil
 }
 
 func handleError(err error) {
@@ -112,7 +176,6 @@ func handleError(err error) {
 		os.Exit(1)
 	}
 }
-
 
 func main() {
 	cfg, err := config.Read()
@@ -127,8 +190,9 @@ func main() {
 	myCommands.names = make(map[string]func(*state, command) error, 5)
 	myCommands.register("login", handlerLogin)
 	myCommands.register("register", handlerRegister)
-  myCommands.register("reset", handlerReset)
-  myCommands.register("users", handlerUsers)
+	myCommands.register("reset", handlerReset)
+	myCommands.register("users", handlerUsers)
+	myCommands.register("agg", handlerAgg)
 	args := os.Args
 	if len(args) < 2 {
 		handleError(fmt.Errorf("Not enough arguments provided"))
